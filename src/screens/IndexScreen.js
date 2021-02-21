de54bec75869c3009eacb71523d7752e7bc6b5e1
Muhammad Ehsan <ehsan@getfieldforce.com>
2021-02-22 00:05:19 +0500

    Setup react navigation.

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { Text, StyleSheet, View, Button } from "react-native"
+
+const IndexScreen = (props) => {
+
+    return <View>
+        <Text>Index screen</Text>
+    </View>
+
+}
+
+const styles = StyleSheet.create({})
+
+export default IndexScreen
