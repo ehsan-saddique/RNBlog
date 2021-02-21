9fb61b70cd8c81a53124dc5f78952179d0f67016
Muhammad Ehsan <ehsan@getfieldforce.com>
2021-02-22 00:19:11 +0500

    Setup global app provider component.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { createAppContainer } from 'react-navigation';
+import React from "react"
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 
@@ -14,4 +15,8 @@ const navigator = createStackNavigator({
   }
 )
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator)
+
+export default () => {
+  return <App />
+}
